7dbfc230cf7a0a5372dae5e759f6d15357e628d8
amananurag <amananurag.20@gmail.com>
2025-07-31 16:19:39 +0530

    db connect

--- index.js
+++ index.js
@@ -1,41 +1,58 @@
-const express= require("express");
-const app= express();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
+//mongodb+srv://amananurag20:d4cbOLDUr6xWSJPy@cluster0.gcimphc.mongodb.net/
 
-app.use(express.json());//json->object
-app.use(express.urlencoded({extended:true}));
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://amananurag20:d4cbOLDUr6xWSJPy@cluster0.gcimphc.mongodb.net/"
+    );
 
-app.get("/user/:id/:name",(req,res)=>{ 
-    
-    console.log(req.params)
-        
-    res.send("dynamic route hai")
+    console.log("db connected");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+dbConnect();
+
+app.use(express.json()); //json->object
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/user/:id/:name", (req, res) => {
+  console.log(req.params);
+
+  res.send("dynamic route hai");
 });
 
-app.get("/home",(req,res)=>{
-   
-    console.log(req.query)
-    res.send("<h1>home route</h1>")
-})
+const m1 = (req, res, next) => {
+  console.log("hi");
+  req.city = "delhi";
 
-app.post("/user",(req,res)=>{ 
-    console.log(req.body)  
-    res.send("<h1>hello how r u</h1>")
+  next();
+};
+
+app.get("/home", m1, (req, res) => {
+  console.log("byee");
+  console.log(req.city);
+  res.send("<h1>home route</h1>");
 });
 
-app.delete("/user",(req,res)=>{   
-    res.send("tata")
+app.post("/user", (req, res) => {
+  console.log(req.body);
+  res.send("<h1>hello how r u</h1>");
 });
 
+app.delete("/user", (req, res) => {
+  res.send("tata");
+});
 
+app.get("/", (req, res) => {
+  res.send("<h1>byeee</h1>");
+});
 
-
-app.get("/",(req,res)=>{
-
-    res.send("<h1>byeee</h1>")
-})
-
-
-app.listen(5000,function(){
-    console.log("server is running on port 5000")
-})
+app.listen(5000, function () {
+  console.log("server is running on port 5000");
+});
